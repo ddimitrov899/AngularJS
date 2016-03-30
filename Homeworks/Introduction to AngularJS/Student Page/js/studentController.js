be8f3434b45857aeaf60155f277a2f7d498ff77f
ddimitrov899 <d.dimitrov899@gmail.com>
2016-03-30 14:46:38 +0300

    add Homework Introduction to AngularJS

---
+++ Homeworks/Introduction to AngularJS/Student Page/js/studentController.js
@@ -0,0 +1,18 @@
+app.controller('studentController', function ($scope) {
+    $scope.student = [
+        {
+            "name": "Pesho",
+            "photo": "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
+            "grade": 5,
+            "school": "High School of Mathematics",
+            "teacher": "Gichka Pesheva"
+        },
+        {
+            "name": "Mara",
+            "photo": "https://www.planwallpaper.com/static/images/Winter-Tiger-Wild-Cat-Images.jpg",
+            "grade": 6,
+            "school": "High School of PGEA",
+            "teacher": "Sofka Jdrambeva"
+        }
+    ];
+});
