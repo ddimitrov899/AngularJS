c8625d18952300e34d4ac615a64fc91d36b6ed29
ddimitrov899 <d.dimitrov899@gmail.com>
2016-04-06 16:03:33 +0300

    add LoginController and RegisterController

--- Project/Project IssueTracker/js/controllers/HomeController.js
+++ Project/Project IssueTracker/js/controllers/HomeController.js
@@ -1,3 +1,10 @@
 app.controller('HomeController', ['$scope', function ($scope) {
+    $scope.login = function (user) {
+        console.log(user);
+    };
+
+    $scope.register = function (user) {
+        console.log(user);
+    };
 
 }]);
