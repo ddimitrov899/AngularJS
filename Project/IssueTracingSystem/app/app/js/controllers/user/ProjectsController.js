46fc3905f187e4daa3996fe305e37778708df202
ddimitrov899 <d.dimitrov899@gmail.com>
2016-04-20 17:47:53 +0300

    add ProjectsController, filter the issue and list project

---
+++ Project/IssueTracingSystem/app/app/js/controllers/user/ProjectsController.js
@@ -0,0 +1,32 @@
+app.controller('ProjectsController', ['$scope', '$location', 'projectService', 'issueService', 'authentication', 'notifyService',
+    function ($scope, $location, projectService, issueService, authentication, notifyService) {
+        $scope.readyDownload = false;
+        var result = [];
+        var currentId = 0;
+
+        var isLogged = authentication.getUser();
+        if (!isLogged) {
+            notifyService.showError('Please login first.');
+            $location.path('/');
+        }else {
+            projectService.getAllProject().then(function (data) {
+                $scope.projects = data.data;
+                console.log(data);
+            });
+        }
+        $scope.selectedId = function (id) {
+
+            if (id != undefined && id !== currentId) {
+                currentId = id;
+
+                issueService.getIssuesById(id).then(function (data) {
+                    result.push(data.data);
+                     $scope.issues = result;
+                    console.log(data);
+                    $scope.readyDownload = true;
+                    result = [];
+                })
+            }
+        }
+
+    }]);
